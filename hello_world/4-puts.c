a9d0430fd18014f19b116c7e22d8bdf8b26a601a
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-20 09:40:24 -0700

    4ajsasjsajsa

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  * main - to print using puts function
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
