7267c2d06437f6bdcde8a87732c768f1c07f9a71
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-21 08:26:07 -0700

    3_kinda

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alphabet soups
+ *
+ * Return:(0)
+ */
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
