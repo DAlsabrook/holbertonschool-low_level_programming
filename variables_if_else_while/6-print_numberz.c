70e1d77c0e4ba9c1e1ca4dd45f9aa0a297516298
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-21 09:23:06 -0700

    idk

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - zero through nine no char
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
