21fc08c2a4fadf5e190cae61ef28fa6031cfcea4
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-21 20:08:13 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - finds if n is >, <, or == 0
+ * @n: the paramiter passed in
+ *
+ * Return: 1 if n > 0 prints '+', 0 if n == 0 prints '0', if -1 < 0 prints '-'
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	 	_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
