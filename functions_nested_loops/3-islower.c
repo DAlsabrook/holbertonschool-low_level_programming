a2f92d358377fdd3fc53e5df8c819027a637b6de
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-21 19:01:21 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdlib.h>
+/**
+ * _islower - checks for lower case letters
+ *
+ * Return: 1 is lower is present. 0 otherwise.
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (islower(c) == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
