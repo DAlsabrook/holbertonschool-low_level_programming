889d5d0047a06b303ce7994cc4b9aeb03fbd377c
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-22 08:48:03 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,34 @@
+#include "main.c"
+/**
+ * _abs - give absolute value
+ * @n: number passed in
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	int p;
+
+	if (n > 0)
+	{
+		for (p = 0; p <= n; p++)
+		{
+			if (p == n)
+			{
+				_putchar(p);
+			}
+		}	
+	}
+	else if (n < 0)
+	{
+		for (p = 0; p >= n; p--)
+		{
+			if (p == n)
+			{
+				_putchar(p)
+			}
+		}
+	}
+
+	return (0);
+}
