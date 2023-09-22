0df32abe06956d5b90e657b3c5cb02b3969d2a1e
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-21 18:22:47 -0700

    123

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef _putchar
-#define _putchar
+#ifndef _putchars
+#define _putchars
 
-int _putchar(char c);
+int _putchar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
