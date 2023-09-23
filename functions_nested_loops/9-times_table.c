7e790a6e111b2e6d56232e3b14e4e5c3ac9c869f
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-23 13:20:04 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ *
+ * return: void
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 10; y++)
+		{
+			_putchar(x * y + '0');
+		}
+		_putchar('\n');
+	}
+}
