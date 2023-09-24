1cedcfae07da927d8e363a847a0d8f749520d590
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-24 09:24:46 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two ints
+ * @a: first int given
+ * @b: second int given
+ *
+ * Return: returns the sum of two ints
+ */
+int add(int a, int b)
+{
+	int ans;
+
+	int ans = a + b;
+
+	return (ans);
+}
