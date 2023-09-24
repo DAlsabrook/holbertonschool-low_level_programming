0ac0a7d16bab4e05107ae108675c795d31cc78fd
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-24 09:38:07 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all ints from given number to 98
+ * @n: the number given to start the count
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	return (0);
+}
