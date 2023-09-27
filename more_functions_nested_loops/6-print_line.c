8a8e921f1ab082ffcbe608b6048eacf1f0ac15c1
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 09:31:06 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: number of line to be printed
+ *
+ * return: void
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar("_");
+		}
+	}
+	_putchar('\n');
+}
