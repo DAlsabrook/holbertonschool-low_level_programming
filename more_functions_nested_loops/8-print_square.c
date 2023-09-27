90f8d48700fa386b165a8ae7237cfa017482463b
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 11:40:08 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ *
+ * return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int c;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size < 0)
+	{
+		_putchar('\n');
+	}
+}
