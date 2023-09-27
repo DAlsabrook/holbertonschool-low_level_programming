84152d4eefbd42e378608bab7bc02871ea005830
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 09:55:15 -0700

    7-print_diagnal.c

---
+++ more_functions_nested_loops/7-print_diagnal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line
+ * @n: number of times slash prints
+ *
+ * return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int s;
+
+	if (n < 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			while (s <= i)
+			{
+				_putchar(' ');
+				s++
+			}
+			_putchar('/');
+		}
+	}
+	_putchar('\n');
+}
