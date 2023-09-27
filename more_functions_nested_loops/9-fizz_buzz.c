cc384e93a05e35032b81a477c0f14cd2486b6cfd
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 11:59:55 -0700

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - number divisible by 3 prints fizz, divisible by prints buzz
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			if ((i % 5) == 0)
+			{
+				printf("FizzBuzz");
+				printf(" ");
+			}
+			else
+			{
+				printf("Fizz");
+				printf(" ");
+			}
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+}
