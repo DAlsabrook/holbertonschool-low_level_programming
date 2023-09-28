5962d42ac3a5d088501c49aa073cc3cb334193a3
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-28 15:07:42 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - prints send half of string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int k;
+
+	j = _strlen(str) - 1;
+	k = j / 2;
+
+	for (i = k; i <= j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
