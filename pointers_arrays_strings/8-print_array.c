2d2ec72595fa5ced40da36581dcab2554ce6fe8e
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-28 15:17:45 -0700

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+#include <stdio.h>
+/**
+ * print_array - prints n number of chars from string
+ * @a: string to br printed
+ * @n: number of chars printed from the string
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
