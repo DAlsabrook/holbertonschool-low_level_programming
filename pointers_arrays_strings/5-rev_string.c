712ffb81ac976250d04a8df467423864867a7758
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 19:11:35 -0700

    5maybe

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - returns reversed string
+ * @s: string to be reversed
+ *
+ * Return: reversed string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int n;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		continue;
+	}
+	n = i - 1;
+	for (i = n; i >= 0; i--)
+	{
+		*s[i] = s[i];
+	}
+}
