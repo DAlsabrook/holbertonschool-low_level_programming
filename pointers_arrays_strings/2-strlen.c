dc591496e0bdbe783de32dea0f0791d242423d8c
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-27 18:08:13 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - gets the length of a string
+ * @s: string passed in
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i <= *s; i++)
+	{
+		_putchar(i + '0');
+	}	
+}
