31a761c62885850aee82e0da6674c16b5f254173
David Alsabrook <Dfalsabrook@gmail.com>
2023-09-28 09:58:44 -0700

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,16 @@
  */
 void rev_string(char *s)
 {
-	int i;
+	int i, c;
 	char* temp;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		temp[i] = s[i];
 	}
-	for (i = i - 1; i >= 0; i--)
+	for (c = i - 1; c >= 0; c++)
 	{
-		*s[i] = temp[i];
+		*s[c] = temp[i];
+		i--;
 	}
 }
