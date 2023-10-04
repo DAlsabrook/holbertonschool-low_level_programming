5c79d60880fcee76aae9d83ee81827e5824ea63a
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-04 12:35:34 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * cap_string - capitalize first letter of every word
+ * @c: string to work with
+ *
+ * Return: string with caps
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if ((i = 0 && (c[i] >= 'a' && c[i] <= 'z')) || c[i] - 1 == ' ')
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+
+	return (c);
+}
