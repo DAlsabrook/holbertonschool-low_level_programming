45594b797e1a46d252453eacef8fe7b21b482022
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-04 08:55:03 -0700

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int dlen = _strlen(dest);
 	int slen = _strlen(src);
 	int i;
 
 	for (i = 0; i < slen && i < n; i++)
 	{
 		dest[i] = src[i];
-		dlen++;
 	}
 
 	return (dest);
