66409291a0b177eb0bd91b7ffaab2841604ce163
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-04 11:12:09 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compares strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2)
+	{
+		return (15);
+	}
+	else if (s1 == s2)
+	{
+		return (0);
+	}
+	else if (s1 < s2)
+	{
+		return (-15);
+	}
+}
