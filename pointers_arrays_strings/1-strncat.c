029df467a53f3b525f4121064e503af049573a4b
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-04 08:12:17 -0700

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strncat - concats two strings
+ * @dest: destinations
+ * @src: source
+ * @n: idk
+ *
+ * Return: new string
+ */
+char *_strncat(char *dest, char*src, int n)
+{
+	int dlen = _strlen(dest);
+	int slen = _strlen(src);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	resturn (dest);
+}
