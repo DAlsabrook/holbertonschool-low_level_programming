70227ee55e774f65e1c69645b4544d1965523e4d
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-05 09:41:28 -0700

    idk

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] <= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return ((char *) s + i);
+		}
+		else if (s[i] == '\0')
+		{
+			return ("");
 		}
 	}
 
