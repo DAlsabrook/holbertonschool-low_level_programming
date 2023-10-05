3637086adb67de5a0571029fb5e8136a2a72dd4b
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-05 11:26:00 -0700

    3final

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,24 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 	unsigned int count = 0;
-
+	int no_match = 0;
 	for (i = 0; s[i]; i++)
 	{
+		if (no_match == 1)
+		{
+			break;
+		}
+
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
 				break;
+			}
+			else
+			{
+				no_match++;
 			}
 		}
 	}
