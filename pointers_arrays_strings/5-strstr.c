83d6a176f740e625ffb505d6a5f5ea68c59db953
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-05 18:37:06 -0700

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdref.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look through
+ * @needle: string trying to find
+ *
+ * Return: new string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	int k, l;
+	int count = 0
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i] == needle[l])
+			{
+				for (k = i, l = j; (haystack[k] == needle[l]) && (needle[l] != '\0'); k++ && l++)
+				{
+					count += 1;
+				}
+				if (count == needle[l])
+				{
+					return ((char *) haystack + i);
+				}
+			}
+		}
+	}
+	return (NULL);
+}
