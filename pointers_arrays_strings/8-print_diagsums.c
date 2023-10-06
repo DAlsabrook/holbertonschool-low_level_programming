217cf3242af9e330c2f142777dd47c8a6f5b0815
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-05 20:27:53 -0700

    7_i_guess

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio>
+/**
+ * print_diagsums - prints the diagnal s
+ * @a: square array
+ * @size: size of square array
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int row, column, count, sumDown, i;
+	int nums[];
+	
+	count = 0;
+	sumDown = 0;
+
+	for (row = 0; row <= size; row++)
+	{
+		for (column = 0; column <= size; column++)
+		{
+			if (column == count)
+			{
+				nums[column] = a[row][column];
+			}
+		}
+		count++;
+	}
+	
+	for (i = 0; i <= size; i++)
+	{
+		if (nums[i] != '\0')
+		{
+			sumDown += nums[i];
+		}
+	}
+	printf("%d, ", sumDown);
+}
