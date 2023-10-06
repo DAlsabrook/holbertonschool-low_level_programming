f8c6d2aaf890913810669b6f4dd5c44f803c40f5
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-06 10:51:29 -0700

    8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints the diagnal s
+ * print_diagsums - prints the diagnal
  * @a: square array
  * @size: size of square array
  *
@@ -9,23 +9,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-        int row, count;
-        int down;
+        int row;
+        int down, up;
 
         down = 0;
-        count = 1;
+        up = 0;
 
-        for (row = 0; row <= size * size; row++)
+        for (row = 0; row < size; row++)
         {
-                if (row % size == 0 && row != 0)
-                {
-                        down += a[row + count];
-                        count++;
-                }
-                else if (row == 0)
-                {
-                        down += a[0];
-                }
+		down += a[(size * row) + row];
+		up += a[(size * (row + 1) - (row + 1))];
         }
-        printf("%d, ", down);
+        printf("%d, %d\n", down, up);
 }
