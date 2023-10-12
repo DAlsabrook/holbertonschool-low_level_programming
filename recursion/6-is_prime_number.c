097575d01987775570d8711fc8c40a579223acbd
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 11:03:45 -0700

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * is_prime_number - finds if number is prime
+ * @n: number to find prime
+ *
+ * Return: 1 if prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return(0);
+	}
+	return (prime_time(n, 2));
+}
+
+/**
+ * prime_time - recursivly finds prime number
+ * @n: number to find if prime
+ * @i: iterater
+ *
+ * Return: 1 if prime and 0 if not
+ */
+int prime_time(int n, int i)
+{
+	if (n % i = 0)
+	{
+		return (0);
+	}
+	else if (n % i != 0)
+	{
+		return(prime_time(n, ++i));
+	}
+	return (1);
+}
