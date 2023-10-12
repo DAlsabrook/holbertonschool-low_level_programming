c4b98190d88702f143fe0322a7e93da6188c6a96
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 09:00:59 -0700

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - find string length with recursion
+ * @s: string being passed in
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+ 	if(*s == '\0')
+	{
+		return(len);
+	}
+	else
+	{
+		len++;
+	}
+}
