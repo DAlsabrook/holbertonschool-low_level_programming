ca114d44ecf29d6d812e0a0e1c92aad611902cc8
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 10:41:10 -0700

    5_incriment_up

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -23,11 +23,11 @@ int find_root(int num, int root)
 {
 	if (root * root > num)
 	{
-		return (1);
+		return (-1);
 	}
 	else if (root * root == num)
 	{
 		return (root);
 	}
-	return (find_root(num, root - 1));
+	return (find_root(num, ++root));
 }
