daee38045aa5304e94794794bbf180e875659bf8
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 20:54:42 -0700

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints addition of arguments
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (succes) 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int nums = 0;
+	int result = 0;
+
+	if (argc = 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(atoi(argv[i])))
+			{
+				result += atoi(argv[i]);	
+			}
+			else
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+	}
+
+	return (0);
+}
