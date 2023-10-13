f5230d287d79b87b40da482c60fc28aa67b39563
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 19:30:20 -0700

    0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - runs the program
+ *
+ * Return: name of program
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Directory going over argc and argv.
