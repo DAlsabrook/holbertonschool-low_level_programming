0ac519551cf725c89459709568db002277042a4f
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 21:27:37 -0700

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			for (j = 0, len = strlen(argv[i]); j < len; j++)
+			for (j = 0, len = strlen(argv[i]) - 1; j < len; j++)
 			{
 				if (isdigit(argv[i][j]) && argv[i][j] != '\0')
 				{
