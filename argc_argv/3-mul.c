f722b3d3801573786f7ad448e10ebf5b1861fa0f
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 20:31:23 -0700

    2

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			nums = strtol(argv[i], NULL, 10);
-			result *= nums;	
+			result *= nums;
 		}
 		printf("%d\n", result);
 	}
