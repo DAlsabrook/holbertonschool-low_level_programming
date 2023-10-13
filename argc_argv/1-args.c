7b17f77152fd430e9455e9befe34a2c671a89690
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-12 19:37:59 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - gets amount of arguments passed into it
+ * @argc: count of arguments
+ *
+ * Return: number of arguments
+ */
+int main(int argc)
+{
+	printf("%d", argc);
+	printf("\n");
+
+	return (0);
+}
