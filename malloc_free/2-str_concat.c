82abf713450006e3fbcc547b48d5535f6a435577
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-19 14:42:35 -0500

    new_c_fix

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * str_concat - Concatonated 2 strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: new string
+ */
+char *str_concat(char *s1, char* s2)
+{
+
+}
