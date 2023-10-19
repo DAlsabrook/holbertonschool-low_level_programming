c35dd829c8be42167decc230bf4148251eceb086
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-19 14:16:33 -0500

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strdup - copies a str to a new location
+ * @str: string to copy
+ *
+ * Return: char* to new string copy
+ */
+char *_strdup(char *str)
+{
+	int len, i;
+	char *str_cp;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		continue;
+	}
+	
+	str_cp = malloc(sizeof(char) * i + 1)
+	
+	if (str == NULL || str_cp == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		str_cp[i] = str[i];
+	}
+
+	return (str_cp);
+
+}
