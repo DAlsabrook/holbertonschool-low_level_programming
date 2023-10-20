01a8855e3c30ef749d11b180d69f8c2adce5fa52
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-20 13:11:51 -0500

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * free_grid - frees grid
+ * @grid: grid to be freed
+ * @height: height of grid to be freed
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i <= height; i++)
+	{
+		free(grid[i]);
+	}
+}
