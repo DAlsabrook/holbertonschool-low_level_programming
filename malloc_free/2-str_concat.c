fe236df0f9329db1379dcc567a8e5d1403631e9f
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-20 09:43:23 -0500

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char* s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = " ";
 	}
 	else if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = " ";
 	}
 
 	new_str = malloc(sizeof(char) * strlen(s1) + strlen(s2) + 1);
