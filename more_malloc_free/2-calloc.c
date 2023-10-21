af52dd3f8bab781018ff1635638aa29ec424f1b0
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-21 16:44:20 -0500

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr;
+	int *arr, i;
        
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +17,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (arr == NULL)
 		return (NULL);
-
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
 	return (arr);
 }
