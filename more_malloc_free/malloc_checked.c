50e5d4e830937f976f0257fc99b4be1843e80e64
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-21 14:32:59 -0500

    0

---
+++ more_malloc_free/malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: int used to allocate memory
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *res = malloc(b);
+
+	if (res == NULL)
+	{
+		return;
+	}
+
+	return (res);
+}
