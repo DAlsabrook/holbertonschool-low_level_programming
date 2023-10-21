5f21f29c45d6a366b6b50d1de3a96a118a510dfe
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-21 15:39:06 -0500

    1again

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *new_str;
 
-	new_str = (char *)malloc((strlen(s1) - 1) + strlen(s2));
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,6 +20,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+
+	new_str = (char *)malloc((strlen(s1) - 1) + strlen(s2));
 	if (new_str == NULL)
 	{
 		return (NULL);
