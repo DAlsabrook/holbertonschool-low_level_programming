3ae5449ddde88232b63a26cb8f7cbae83e10e780
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-22 12:03:47 -0500

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -8,18 +8,18 @@ int *array_range(int min, int max)
 {
 	int i = 0;
 	int *arr;
-	int j;
+	int range;
 
 	if (min > max)
 		return (NULL);
 	while (i < max)
 		i++;
-	j = i - min;
-	arr = (int *)malloc(sizeof(int) * j * 2);
+	range = i - min;
+	arr = (int *)malloc(sizeof(int) * range + 1);
 
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i <= j; i++)
+	for (i = 0; i <= range; i++)
 	{
 		arr[i] = min;
 		min++;
