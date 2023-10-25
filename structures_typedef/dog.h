b042f8721cbb9199e354f08c878bce4c0cfcc33f
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-25 10:38:42 -0500

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog type
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Description: dog type animal
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
