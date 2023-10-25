ccd88f98d60a816c68dde543737b1188585b36c8
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-25 14:29:48 -0500

    5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - frees all memory that frees dog
+ * @d: dog to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+}
