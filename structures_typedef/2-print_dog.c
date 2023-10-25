4128bac4097b8847cc6155f2fe913e38a4c93ab2
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-25 12:36:51 -0500

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,16 +12,16 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name:(nil)\n");
 		else
-			printf("Name:%s\n", name);
+			printf("Name:%s\n", d->name);
 
 		if (d->age < 0)
 			printf("Age:(nil)\n");
 		else
-			printf("Age:%f\n", age);
+			printf("Age:%f\n", d->age);
 
 		if (d->owner ==NULL)
 			printf("Owner:(nil)");
 		else
-			printf("%s\n", owner);
+			printf("%s\n", d->owner);
 	}
 }
