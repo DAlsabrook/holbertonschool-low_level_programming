1eb4405ebc97c599c175937daa545f65fdd38f0c
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-25 11:35:49 -0500

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,10 +10,5 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d =
-	{
-		.name = name,
-		.age = age,
-		.owner = owner
-	};
+	.name = name, .age = age, .owner = owner;
 }
