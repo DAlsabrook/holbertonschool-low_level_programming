fb1b6da0a1ed3af49649f9d304ff71c0b63b1d9e
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-26 13:25:20 -0500

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,10 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res;
 
-	if (array == NULL || cmp == NULL)
-		return;
 	if (size < 0)
-		return (-1)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		res = cmp(array[i]);
