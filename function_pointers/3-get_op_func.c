3f56cff966d6cc8ccb1e3e3d3631210981eb2777
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-26 14:49:31 -0500

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,10 +1,21 @@
 #include "3-calc.h"
 /**
+ * get_op_func - selects correct function asked by user
+ * @s: operator passed by user as string
  *
- *
- * Return:
+ * Return: pointer to the function that corresponds with user input
  */
-
+int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
 
+	if (s != ops[0][1] || s != ops[1][1] || s != ops[2][1] || s != ops[3][1] || s != ops[4][1])
 }
