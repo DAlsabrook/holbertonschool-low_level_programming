d0c222183cfd498bea419911fdbab54206fe650b
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-26 14:15:50 -0500

    add_func_to_op

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,10 +1,41 @@
 #include "3-calc.h"
 /**
+ * op_add - sum of a and b
+ * @a: First int
+ * @b: Second int
  *
- *
- * Return:
+ * Return: result
  */
-
-{
-
-}
+int op_add(int a, int b) {return (a + b)}
+/**
+ * op_sub - difference between a and b
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: result
+ */
+int op_sub(int a, int b) {return (a - b)}
+/**
+ * op_mul - multiply a and b
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: result
+ */
+int op_mul(int a, int b) {return (a * b)}
+/**
+ * op_div - divide a and b
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: result
+ */
+int op_div(int a, int b) {return (a / b)}
+/**
+ * op_mod - modulo a and b
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: result
+ */
+int op_mod(int a, int b) {return (a % b)}
