952a2302b4f073dcc35af8280bcbb35190c8b8f1
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-28 12:39:27 -0500

    first_try

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,11 +6,30 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, funcAddress, result;
+	char *op;
+	op_t calc;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	funcAddress = get_op_func(op);
+
 	if (argc != 3)
 		printf("Error\n");
 		return(98);
 
-	if (argc[2] == "/" || argv[2] == "%" && argv[3] == "0")
+	if (op == "/" || op == "%" && num2 == 0)
 		printf("Error\n");
 		return(100);
+
+	if (funcAddress == NULL)
+		printf("Error\n")
+		return(99);
+
+	calc.f = funcAddress;
+	result = calc.f(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
 }
