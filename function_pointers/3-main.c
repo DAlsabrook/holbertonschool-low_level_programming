af0f7d605be96ba183ee92453aa31c5616c0799a
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-28 12:45:57 -0500

    third_try

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,9 +6,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, funcAddress, result;
+	int num1, num2, result;
 	char *op;
 	op_t calc;
+	int *funcAddress;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
@@ -16,16 +17,20 @@ int main(int argc, char *argv[])
 	funcAddress = get_op_func(op);
 
 	if (argc != 3)
+	{
 		printf("Error\n");
 		return(98);
-
-	if (op == "/" || op == "%" && num2 == 0)
+	}
+	if ((op == '/' || op == '%') && num2 == 0)
+	{
 		printf("Error\n");
 		return(100);
-
+	}
 	if (funcAddress == NULL)
+	{
 		printf("Error\n")
 		return(99);
+	}
 
 	calc.f = funcAddress;
 	result = calc.f(num1, num2);
