bd349b92cc9ec20e00ce9a7259b5e4cd290d0a44
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-28 14:08:18 -0500

    address

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	op = atoi(argv[2]);
 
-	if ((op == '/' && num2 == 0) || (op == '%' & num2 == 0))
+	if ((op == '/' && num2 == 0) || (op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		return(100);
