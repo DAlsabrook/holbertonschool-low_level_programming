cc364b022bb11cb96183285d416990f88573f98a
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 11:16:29 -0500

    1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: separates numbers
+ * @n: number of numbers
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int tmp;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = va_args(args, int)
+		if (separator != NULL)
+		{
+			printf("%d%s ", tmp, separator);
+		}
+		else
+		{
+			printf("%d ", tmp);
+		}
+	}
+
+	va_end(args);
+}
