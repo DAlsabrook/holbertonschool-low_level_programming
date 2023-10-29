52827b475cb9cee3ae97c67cf07fa12379686faa
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 12:00:49 -0500

    2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		tmp = va_arg(args, int);
+		tmp = va_arg(args, char *);
 		if (separator != NULL && *separator != '\0' && i < (n - 1))
 		{
 			printf("%s%s", tmp, separator);
