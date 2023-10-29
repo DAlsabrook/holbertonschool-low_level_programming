764a394a6e245cc3dd54ecd201749a886d103379
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 11:18:10 -0500

    1

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef ARGS
 #define ARGS
 #include <stdarg.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
