e308d6a543d60a361c07d9e65aa493fcdb564035
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 15:11:16 -0500

    test

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_all - prints anything passed in
+ * @format: list of types of arguments passed
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+
+}
