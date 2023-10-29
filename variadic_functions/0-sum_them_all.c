b1cf1b9e82dc77de9b186b93ac5af20b32089926
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 11:02:37 -0500

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,11 +7,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	va_start(args, n);
 	unsigned int i;
 	int x;
 	int sum = 0;
+
+	va_list args;
+	va_start(args, n);
 
 	if (n == 0)
 		return (0);
