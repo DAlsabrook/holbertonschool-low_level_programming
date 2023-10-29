7dac31767ffc40df84c46f80f37bcf715f7e1733
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 10:58:52 -0500

    0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sum all args
+ * @n: count of args
+ *
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	va_start(args, n);
+	int i, x;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+
+	va_end(args);
+
+	return (sum);
+}
