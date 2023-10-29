a343c4dc297b54c7414b97db53b28261a7513858
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-29 16:18:08 -0500

    test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,5 +7,39 @@
  */
 void print_all(const char * const format, ...)
 {
+    va_list args;
+    va_start(args, format);
 
+    char c;
+    int i;
+    float f;
+    char *s;
+    int isStringNull;
+
+    while ((c = *format++) != '\0') {
+        if (c == 'c') {
+            i = va_arg(args, int);
+            printf("%c ");
+        }
+        if (c == 'i') {
+            i = va_arg(args, int);
+            printf("%d ");
+        }
+        if (c == 'f') {
+            f = (float)va_arg(args, double);
+            printf("%f ");
+        }
+        if (c == 's') {
+            s = va_arg(args, char*);
+            isStringNull = (s == NULL);
+            if (isStringNull) {
+                printf("(nil) ");
+            } else {
+                printf("%s ");
+            }
+        }
+    }
+
+    printf("\n");
+    va_end(args);
 }
