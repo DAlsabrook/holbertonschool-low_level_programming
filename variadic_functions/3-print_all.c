ba2e29cbb81dc7934e857f103322876079b12905
David Alsabrook <Dfalsabrook@gmail.com>
2023-10-30 12:26:47 -0500

    4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,40 +7,44 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int j;
-    char c;
-    int i;
-    float f;
-    char *s;
-    int isStringNull;
-	va_start(args, format);
-    j = 0;
-    while ((c = format[j]) != '\0') {
-        if (c == 'c') {
-            i = va_arg(args, int);
-            printf("%c ", i);
-        }
-        if (c == 'i') {
-            i = va_arg(args, int);
-            printf("%d ", i);
-        }
-        if (c == 'f') {
-            f = (float)va_arg(args, double);
-            printf("%f ", f);
-        }
-        if (c == 's') {
-            s = va_arg(args, char*);
-            isStringNull = (s == NULL);
-            if (isStringNull) {
-                printf("(nil) ");
-            } else {
-                printf("%s ", s);
-            }
-        }
-	j++;
-    }
+	int i = 0;
+	char *s;
+	char *sep = "";
 
-    printf("\n");
-    va_end(args);
+	va_list args;
+
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(args);
 }
