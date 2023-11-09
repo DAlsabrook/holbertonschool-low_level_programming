f814d38f51d98155a8c55a9d4401c02b73de11ab
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 19:24:51 +0000

    moving current next = new node out of loop

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,9 +25,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	current = *head;
 	while (current != NULL)
 	{
-		current->next = new_node;
 		current = current->next;
 	}
+	current->next = new_node;
 	new_node->next = NULL;
 	return (new_node);
 }
