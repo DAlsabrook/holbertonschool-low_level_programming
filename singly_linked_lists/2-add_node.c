59a4242654314191c0840e7495cfc7d21640791e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 18:47:51 +0000

    new node next equals head

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,8 +12,8 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return(NULL);
-	new_node = *head;
 	new_node->str = strdup(str);
+	new_node->next = *head;
 	*head = new_node;
 
 	return(new_node);
