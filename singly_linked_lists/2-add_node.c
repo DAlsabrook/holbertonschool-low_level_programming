bc7d65143adba560a52cee174d2b4d290717f9fa
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 18:53:06 +0000

    tring to get the string length

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,6 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
+	new_node->len += strlen(str);
 
 	return(new_node);
 }
