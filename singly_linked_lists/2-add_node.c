36766e006f4f4d8e9c2f2ff73ce03644511985b3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 18:45:32 +0000

    moved if to check new node is null

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,12 +10,11 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = NULL;
 
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return(NULL);
 	new_node = *head;
 	new_node->str = strdup(str);
 	*head = new_node;
-
-	if (new_node == NULL)
-		return (NULL);
 
 	return(new_node);
 }
