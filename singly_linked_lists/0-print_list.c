8b423d5bf55fd7600a81da500b9be90886926596
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 17:33:24 +0000

    got rid of repitition

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,15 +14,13 @@ size_t print_list(const list_t *h)
 		if (h->str != NULL)
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
 		}
 		else
 		{
 			printf("[0] (nil)\n");
-			count++;
-			h = h->next;
 		}
+		count++;
+		h = h->next;
 	}
 
 	return (count);
