02f281e060e4c10bc11d33790eb1ca98205abba4
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 18:30:52 +0000

    first attempt at task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_node - adds node at beggining
+ * @str: string to copy to new node
+ *
+ * Return: adress to new node
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = NULL;
+
+	new_node = malloc(sizeof(list_t));
+	new_node->str = str;
+	*head = new_node;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	return(new_node);
+}
