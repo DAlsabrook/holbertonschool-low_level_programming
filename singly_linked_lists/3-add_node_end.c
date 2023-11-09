690b4f889c7c2e1bf1b6d9ef134dd4e4eab9652b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 20:30:52 +0000

    Fixing betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return(new_node);
+		return (new_node);
 	}
 	current = *head;
 	while (current->next != NULL)
