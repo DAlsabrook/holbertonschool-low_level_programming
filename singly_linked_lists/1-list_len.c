f6bbdc7e339e67c20066f9c243d99be46f5807cc
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-09 17:46:15 +0000

    Adding task 1 first attempt

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - list the length of elements in list_t
+ * @h: head of list
+ * return: count of nodes
+*/
+size_t list_len(const list_t *h)
+{
+    int count = 0;
+
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
