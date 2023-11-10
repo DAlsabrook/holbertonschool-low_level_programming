e8cbe3a5ed758cb7f26ebe41b23f4f6aefeaf264
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 16:26:08 +0000

    Added null return for malloc

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,6 +11,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new = NULL;
 
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
