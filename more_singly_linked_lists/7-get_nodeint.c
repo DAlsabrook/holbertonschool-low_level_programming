d8bd173a9e38f5751e94caef5b57cad9fa01e15c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 19:48:38 +0000

    Changed what current increments to

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	current = head;
 	while (current && i <= index)
 	{
-		current = current->next;
+		current = head->next;
 		i++;
 	}
 
