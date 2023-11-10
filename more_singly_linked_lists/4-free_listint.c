9715f958dce359a3b2adbe9bd8a76faa0b399195
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 16:44:00 +0000

    First attempt task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list of memory
+ * @head: list to free
+ * 
+ * Return: void
+*/
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		free(head->n);
+		free(head);
+	}
+}
