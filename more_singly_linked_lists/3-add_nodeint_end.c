93016bb8f63766d0b24b78462a46aa9aa645c146
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 16:38:31 +0000

    changed while loop conditional syntax

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new);
 	}
 	current = *head;
-	while (!current->next)
+	while (current->next != NULL)
 	{
 		current = current->next;
 	}
