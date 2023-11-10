666c22d0ebd9a7a066b6431db49b15c20574bf40
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 19:55:36 +0000

    changed <= to <

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	current = head;
-	while (current && i <= index)
+	while (current && i < index)
 	{
 		current = head->next;
-		head = current;
 		i++;
 	}
 
