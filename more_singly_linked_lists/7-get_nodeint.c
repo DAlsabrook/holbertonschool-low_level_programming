39ff74951301ee6baf5180d3f571b0073a487f38
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 19:36:09 +0000

    added temp list

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -2,19 +2,22 @@
 /**
  * get_node_at_index - get node at nth index
  * @index: the index starting at 0
+ * @head: list to check
  *
  * Return: adress of node
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *current = NULL;
 	unsigned int i;
 
+	current = head;
 	for (i = 0; i <= index; i++)
 	{
-		if (!head)
+		if (!current)
 			return (NULL);
-		head = head->next;
+		current = current->next;
 	}
 
-	return (head);
+	return (current);
 }
