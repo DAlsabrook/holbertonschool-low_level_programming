cc12ac22564a0b4757be3d1a2fc6037c6befda5f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 19:58:08 +0000

    Change iterator in loop

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	current = head;
 	while (current && i < index)
 	{
-		current = head->next;
+		current = current->next;
 		i++;
 	}
 
