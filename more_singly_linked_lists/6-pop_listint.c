72f47821af9be174be786fcd462edeb9c2220506
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 19:00:46 +0000

    Added tmp list to hold next

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -7,12 +7,15 @@
 */
 int pop_listint(listint_t **head)
 {
+	listint_t *tmp_l;
 	int tmp;
 
 	if(!head || !*head)
 		return(0);
+	tmp_l = (*head)->next;
 	tmp = (*head)->n;
-	*head = (*head)->next;
+	free(*head);
+	*head = (tmp_l)->next;
 
 	return (tmp);
 }
