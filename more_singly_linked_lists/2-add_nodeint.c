27b3804cb3d4a1289f1afc14b7aa0c1de51d9ca2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-10 16:24:18 +0000

    first attempt task 2

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_nodeint -adds new node to beginning
+ * @head: list passed in
+ * @n: int to put in node
+ *
+ * Return: adress of node
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
