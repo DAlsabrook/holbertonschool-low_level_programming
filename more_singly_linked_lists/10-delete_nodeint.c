1b4b6c57533ea016e1d545c6c25e7517b18ee176
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-11 16:05:05 +0000

    return null if head null

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	temp = malloc(sizeof(*temp));
 	if (!temp || !current)
-		return (-1);
+		return (NULL);
 	for (i = 0; i < index; i++)
 	{
 		if (i == index)
