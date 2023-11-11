fa6d3412efed3b82db04f0da3fd1fd5adfb6c886
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-11 15:55:44 +0000

    first attempt at task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * delete_nodeint - deletes node and index
+ * @head: list to use
+ * @index: index to delete
+ * 
+ * Return: 1 on success. -1 on fail.
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *temp;
+	unsigned int i;
+	temp = malloc(sizeof(*temp));
+	if (!temp || !current)
+		return (-1);
+	
+	for (i = 0; i <= index; i++)
+	{
+		if (i == index)
+		{
+			current = current->next;
+			temp->next = current;
+			return (1);
+		}
+		temp = current;
+		current = current->next;
+	}
+	return (-1);
+}
