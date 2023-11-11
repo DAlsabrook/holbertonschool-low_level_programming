c38f792ad696c710595fa2c250dff0da0635eac1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-11 16:42:17 +0000

    freeing current

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -33,6 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			if (index > i)
 				return (-1);
 			temp->next = NULL;
+			free(current);
 			return (1);
 		}
 		temp = current;
