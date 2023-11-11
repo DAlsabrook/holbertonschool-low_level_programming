14fef76b17b147daa8ae8246464141adfe2281a5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-11 14:19:22 +0000

    first attempt at task 8

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - list sum of data from all nodes
+ * @head: list to check
+ * 
+ * Return: sum of data
+*/
+int sum_listint(listint_t *head)
+{
+	listint_t *current = NULL;
+
+	if (head)
+	{
+		return (sum_listint(head->next) + head->n);
+	}
+	return (0);
+}
