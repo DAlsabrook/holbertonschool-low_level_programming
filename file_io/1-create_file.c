a9b9228e330755d8f62b92d218885337e7955372
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 03:34:16 +0000

    first attempt at task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,5 +6,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	
+	int o, w, len = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+	}
+	o = open(filename, O_CREAT, O_RDWR, O_TRUNC);
+	w = write(o, text_content, len);
+	if ((check(o) + check(w)) < 0)
+		return (-1);
+	close(o);
+	return(w);
 }
