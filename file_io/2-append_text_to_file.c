bf2717bb0dedcf884567f4aaf6d823731c25b137
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 03:55:23 +0000

    added else if for errors

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,8 +8,6 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
-	if (!filename)
-		return (-1);
 	if (text_content)
 	{
 		while (text_content[len] != '\0')
@@ -20,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		o = open(filename, O_WRONLY | O_APPEND);
 		w = write(o, text_content, len);
 	}
-	if (o == -1 || w == -1)
+	else if (!filename || o == -1 || w == -1)
 		return (-1);
 	return (1);
 }
