d5feb1573194290a8ae8144bbb594523dd414018
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 04:45:43 +0000

    had error codes switched

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,8 +37,8 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
 	}
 }
 /**
@@ -103,7 +103,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
-	
+
 	free(buffer);
 	close_file(from);
 	close_file(to);
