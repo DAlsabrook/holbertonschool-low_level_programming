13c2bae936feb37440dfc9654e74e38409df697d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 02:36:30 +0000

    first attempt at 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,20 +1,26 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * read_textfile - read a file and write to the POSIX
+ * @filename: name of file to read
+ * @letters: number of letter expected
+ *
+ * Return: number of letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int i;
-	FILE *my_file;
+	int file_num;
+	ssize_t file;
+	char buff[letters];
 
-	my_file = open(filename, O_RDONLY);
+	file_num = open(filename, O_RDONLY);
+	if (file_num == -1)
+		return (0);
+	file = read(file_num, buff ,letters);
+	if (file == -1)
+		return (-1);
+	if (write(1, buff, file) != file)
+		return (-1);
 
-	for (i = 0; i < letters; i++)
-	{
-
-	}
-
-	close(my_file);
+	close(file_num);
+	return (file);
 }
