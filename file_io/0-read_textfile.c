f0b82ccb6a445b5fa43f0135ba3856b851396887
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 03:14:48 +0000

    added STDOUT_FILENO in write function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,13 +12,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 
 	if (filename == NULL)
-		return ((long int)NULL);
-	buff = malloc(sizeof(char) * letters + 1);
+		return (0);
+	buff = malloc(sizeof(char) * letters);
 	if (!buff)
 		return (0);
 	o = open(filename, O_RDONLY);
 	r = read(o, buff, letters);
-	w = write(1, buff, r);
+	w = write(STDOUT_FILENO, buff, r);
 	if ((check(o) + check(r) + check(w)) < 0)
 		return (0);
 	close(o);
