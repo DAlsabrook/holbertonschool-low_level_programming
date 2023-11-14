b946905103e7219eca50c7f6cd3c11b9aefbb174
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 03:58:06 +0000

    betty doc

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
+ * append_text_to_file - append text to a file
+ * @filename: name of file to append
+ * @text_content: text to append on the end of file
  *
- *
- *
+ * Return: 1 on success. -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
