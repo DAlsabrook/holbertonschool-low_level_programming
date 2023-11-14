10bfd5c20232f70a95b0ad64276cf32313f2b5ce
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-14 02:13:25 +0000

    saving building block

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,5 +6,15 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	
+	int i;
+	FILE *my_file;
+
+	my_file = open(filename, O_RDONLY);
+
+	for (i = 0; i < letters; i++)
+	{
+
+	}
+
+	close(my_file);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 char _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
