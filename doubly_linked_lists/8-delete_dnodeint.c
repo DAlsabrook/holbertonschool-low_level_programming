44e56d44fbf95807ef4bcce2b221f3b58777821f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 16:14:44 -0600

    First attempt task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,15 +23,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	while (i < index - 1)
 	{
-		if (!temp || !(temp->next))
-			return (-1);
-		temp = temp->next;
+		current = current->next;
 		i++;
 	}
 
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
+	tmp = current->next->next;
+	current->next = tmp;
+	free(tmp->prev);
+	tmp->prev = current;
 
 	return (1);
 }
