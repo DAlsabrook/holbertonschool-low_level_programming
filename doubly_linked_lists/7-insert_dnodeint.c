6e7daebe8ca5baf58a3e0444af9db1ccece343f6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 15:35:40 -0600

    h = new if idx = 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *h;
+		*h = new;
 		return (new);
 	}
 	/*logic to set the new node at an index*/
