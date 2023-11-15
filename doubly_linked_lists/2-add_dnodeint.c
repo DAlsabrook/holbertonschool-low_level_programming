fb48f4d54b292ca32a4ea743b95065770dd71b6d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 14:15:38 -0600

    typo on new node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new->next = *head;
+	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
