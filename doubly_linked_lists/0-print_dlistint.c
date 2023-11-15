9aeb1dfa0df119636250d53feed42579ebe38040
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 10:38:32 -0600

    Creating file and header

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of dlist
+ * @h: list to use
+ * 
+ * Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+}
