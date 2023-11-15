3455d1ea6aa5da7bc78465511b4cf027001980a1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 14:48:58 -0600

    case for null head

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,12 +11,15 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = NULL;
 	unsigned int i = 0;
 
+	if(!head)
+		return (NULL);
 	current = head;
 	while (current->next && i < index)
 	{
 		current = current->next;
 		i++;
 	}
-
+	if (i < index)
+		return (NULL);
 	return (current);
 }
