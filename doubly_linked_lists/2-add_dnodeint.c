8e7d0e7f2219b2f660ed897847566e06f4615693
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 14:24:28 -0600

    if to handle head null

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = *head;
-	(*head)->prev = new_node;
+	if (*head)
+		(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
