4afda713a0e0f1a5a7bbec87de556e8315795263
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 15:45:46 -0600

    i =1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *tmp, *current;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	/*create new node*/
 	new = malloc(sizeof(dlistint_t));
