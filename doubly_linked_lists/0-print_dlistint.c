e130bf2c3159c88cd2133d1818d5547f96fb0b34
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 10:51:59 -0600

    idk

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,10 +2,10 @@
 /**
  * print_dlistint - prints all elements of dlist
  * @h: list to use
- * 
+ *
  * Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+	return(0);	
 }
