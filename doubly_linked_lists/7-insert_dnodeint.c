79b479515a146cc8870431243adea8b0f4b2a90d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 15:22:12 -0600

    off by 1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	/*logic to set the new node at an index*/
 	current = *h;
-	while (current->next && i < (idx - 1))
+	while (current->next && i <= idx)
 	{
 		current = current->next;
 		i++;
