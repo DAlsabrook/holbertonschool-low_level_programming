9f915a0ae93279c64e2bd0bedcdf31ba1f7a2c3b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 14:13:15 -0600

    Redo

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,32 +2,19 @@
 /**
  * add_dnodeint - adds a node to the list
  * @head: head of list
- * @n: place to add new node
+ * @n: data to put in node
  *
  * Return: new node adress
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	int i;
-	dlistint_t *new_node, *tmp, *current = *head;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-	if (!*head)
-	{
-		(*head)->next = new_node;
-		new_node->prev = *head;
-		return (new_node);
-	}
-	for (i = 0; i < n; i++)
-	{
-		tmp = current;
-		current = current->next;
-	}
-	new_node->next = current;
-	new_node->prev = tmp;
-	current->prev = new_node;
-	tmp->next = new_node;
+	new_node->n = n;
+	*head = new_node;
+
 	return (new_node);
 }
