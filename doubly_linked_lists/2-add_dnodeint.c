a911a9e3c023b09f3ab4524500a8dd1dd84c42f9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 13:57:38 -0600

    changed null if

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node, *tmp, *current = *head;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node || !*head || !n)
+	if (!new_node)
 		return (NULL);
 	for (i = 0; i < n; i++)
 	{
