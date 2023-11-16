902273973331b87fafc3b1b4590cabc567e2ecdf
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 14:19:46 -0600

    free values

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -42,6 +42,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (strcmp(key, current_item->key) == 0)
 	{
+		free(current_item->value);
+		free(current_item->key);
 		free(current_item);
 	}
 
