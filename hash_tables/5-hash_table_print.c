aaadc91f9151d306fcf60de991e922e113fb074c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:28:22 -0600

    put flag to print commas

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,19 +6,22 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int row;
+	unsigned long int row , flag;
 	hash_node_t *current;
 	if (!ht)
 		return;
 	putchar('{');
 	for (row = 0; row < ht->size; row++)
 	{
+		if (flag == 1)
+			printf(", ");
 		current = ht->array[row];
 		while (current)
 		{
 			printf("'%s': '%s'", current->key, current->value);
 			current = current->next;
 		}
+		flag = 1;
 	}
 	putchar('}');
 	return;
