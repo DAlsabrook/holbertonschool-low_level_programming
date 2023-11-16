e8eacdd312419658256a46baf7f6ca0da8769141
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:11:02 -0600

    changed how to print commas

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -16,11 +16,10 @@ void hash_table_print(const hash_table_t *ht)
 		current = ht->array[row];
 		while (current)
 		{
-			if (row >= ht->size && !current->next)
-				printf("'%s': '%s'", current->key, current->value);
-			else
-				printf("'%s': '%s', ", current->key, current->value);
+			printf("'%s': '%s'", current->key, current->value);
 			current = current->next;
+			if (current)
+				printf(", ");
 		}
 	}
 	putchar('}');
