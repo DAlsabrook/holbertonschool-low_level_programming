7beff49e271e94017a47733269f317301426d94b
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 15:01:19 -0600

    free whats not used

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -42,6 +42,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		item->next = current_item;
 	}
+	else
+	{
+		free(current_item->value);
+		free(current_item->key);
+		free(current_item);
+	}
 
 	return (1);
 }
