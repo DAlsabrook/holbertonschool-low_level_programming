4a25affb33661496fa3c79282e1f56f003500ff4
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 12:59:57 -0600

    fix formatt to print

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,14 +10,19 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *current;
 	if (!ht)
 		return;
+	putchar('{');
 	for (row = 0; row < ht->size; row++)
 	{
 		current = ht->array[row];
 		while (current)
 		{
-			printf("'%s': '%s', ", current->key, current->value);
+			if (row == ht->size && !current->next)
+				printf("'%s': '%s'");
+			else
+				printf("'%s': '%s', ", current->key, current->value);
 			current = current->next;
 		}
 	}
+	putchar('}');
 	return;
 }
