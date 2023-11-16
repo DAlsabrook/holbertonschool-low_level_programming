60fec1ef13a48ceb6900687606b46f17698c49fd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 19:02:24 -0600

    else to else if

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -49,7 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = tmp;
 		new->prev = current;
 	}
-	else
+	else if (!current->next)
 	{
 		current->next = new;
 		new->prev = current;
