1448177bfd3e2737633c1d82af5497024e94f6dd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:33:28 -0600

    added if

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -13,15 +13,18 @@ void hash_table_print(const hash_table_t *ht)
 	putchar('{');
 	for (row = 0; row < ht->size; row++)
 	{
-		if (flag == 1)
-			printf(", ");
-		current = ht->array[row];
-		while (current)
+		if (ht->array[row])
 		{
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
+			if (flag == 1)
+				printf(", ");
+			current = ht->array[row];
+			while (current)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+				current = current->next;
+			}
+			flag = 1;
 		}
-		flag = 1;
 	}
 	putchar('}');
 	return;
