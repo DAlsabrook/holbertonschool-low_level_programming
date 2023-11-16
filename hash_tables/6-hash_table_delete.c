7d0700e95f2207e63c7e891d7146088130f2b6f1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:46:42 -0600

    First attempt at task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,28 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ * hash_table_delete - deletes a hash table
+ * @ht: table to use
+ * 
+ * Return: void
  */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *table = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int row;
+	/*loop through table rows*/
+	for (row = 0; row < ht->size; row++)
+	{
+		node = ht->array[row];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+	free(table->array);
+	free(table);
+}
