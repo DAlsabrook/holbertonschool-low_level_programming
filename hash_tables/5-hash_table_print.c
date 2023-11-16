01b0c62dcfedbc26e6fcd1027e4e3cfbef07041c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:39:20 -0600

    betty errors + documentation

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,13 +1,15 @@
 #include "hash_tables.h"
 /**
+ *hash_table_print - prints elements in hash
+ @ht: table to use
  *
- *
- *
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int row , flag = 0;
+	unsigned long int row, flag = 0;
 	hash_node_t *current;
+
 	if (!ht)
 		return;
 	putchar('{');
@@ -29,5 +31,4 @@ void hash_table_print(const hash_table_t *ht)
 		}
 	}
 	printf("}\n");
-	return;
 }
