fef7422d12d41cc8d3833afcd4c4a8507e8bb668
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 14:29:54 -0600

    k

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,25 +15,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
-	/*create item and allocate memory for item and values inside item*/
 	item = malloc(sizeof(hash_node_t));
-	item->key = malloc(strlen(key) + 1);
-	item->value = malloc(strlen(value) + 1);
-	if (!item || !item->value || !item->key)
+	if (!item)
 	{
 		free(item);
 		return (0);
 	}
-	/*assign item values*/
+	item->key = malloc(strlen(key) + 1);
+	if (!item->key)
+	{
+		free(item->key);
+		return (0);
+	}
+	item->value = malloc(strlen(value) + 1);
+	if (!item->value)
+	{
+		free(item->value);
+		return (0);
+	}
 	strcpy(item->key, key);
 	if (!item->key)
 		free(item->key);
 	strcpy(item->value, value);
 	if (!item->value)
 		free(item->value);
-	/*get index with key*/
 	index = key_index((const unsigned char *)key, table->size);
-	/*check if index has an item and if so move it over in the array*/
 	current_item = table->array[index];
 	table->array[index] = item;
 	if (current_item && strcmp(key, current_item->key) != 0)
@@ -43,7 +49,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strcmp(key, current_item->key) == 0)
 	{
 		free(current_item->value);
-		free(current_item->key);
 		free(current_item);
 	}
 
