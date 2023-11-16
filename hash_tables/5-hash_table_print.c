6d2edc08974c468c233b560bb640ee2848beb87d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 12:55:33 -0600

    First attempt at task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,3 +4,20 @@
  *
  *
  */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int row;
+	hash_node_t *current;
+	if (!ht)
+		return;
+	for (row = 0; row < ht->size; row++)
+	{
+		current = ht->array[row];
+		while (current)
+		{
+			printf("'%s': '%s', ", current->key, current->value);
+			current = current->next;
+		}
+	}
+	return (NULL);
+}
