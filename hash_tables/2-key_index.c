7506ff9b702ca8f13fd9922a11e35c377ad8c428
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 09:58:23 -0600

    Guess on task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,6 +1,17 @@
 #include "hash_tables.h"
 /**
+ * key_index - 
+ * @key: key
+ * @size: size of array of hash table
  *
- *
- *
+ * Return: index which the key/value was stored
  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int key_index;
+	
+	key_index = hash_djb2(key);
+	key_index %= size;
+
+	return (size);
+}
