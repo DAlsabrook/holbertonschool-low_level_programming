1bc3bcbf8cc2664edb08be63fd17996fafa5c77a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:13:42 -0600

    figuring out commas

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			printf("'%s': '%s'", current->key, current->value);
 			current = current->next;
-			if (current)
+			if (current->next)
 				printf(", ");
 		}
 	}
