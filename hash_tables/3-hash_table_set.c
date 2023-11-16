bb44502a3b4d8358ec8967aa130e067462f92f16
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 10:21:33 -0600

    First attempt

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,27 @@
 #include "hash_tables.h"
 /**
+ * hash_table_set - add an element to the hash table
+ * @value: data for inserted node
+ * @key: key to use
+ * @ht: table to use
  *
- *
- *
+ * Return: 1 on success. 0 on failure.
  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int i;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->value = value;
+	new_node->key = key_index(key, ht->size);
+	
+	for (i = 0; i < new_node->key; i++)
+	{
+		continue;
+	}
+	ht->array[i] = new_node;
+	return (1);
+}
