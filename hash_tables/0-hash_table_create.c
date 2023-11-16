9a37adabc44f98e1425b63db792f47995a429ca2
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-15 22:45:28 -0600

    first attempt at hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,25 @@
 #include "hash_tables.h"
 /**
+ * hash_table_create - creates hash table
+ * @size: size of table
  * 
- * 
- * 
+ * Return adress of table
 */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t*));
+
+	for (i = 0; i < table->size; i++)
+	{	
+		table->array[i] = NULL;
+	}
+
+	return (table);
+}
