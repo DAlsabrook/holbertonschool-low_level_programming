b231be412e467eb3edd1a2ebde1ecacbdc4475b9
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 13:06:09 -0600

    changed if for if current->next is null

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -16,7 +16,7 @@ void hash_table_print(const hash_table_t *ht)
 		current = ht->array[row];
 		while (current)
 		{
-			if (row == ht->size && !current->next)
+			if (row >= ht->size && !current->next)
 				printf("'%s': '%s'", current->key, current->value);
 			else
 				printf("'%s': '%s', ", current->key, current->value);
