0d2cf3d1e53999794b4196998dba12de5d2d9c1e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 11:21:16 -0600

    case for same key multiple times

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,10 +28,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(item->value, value);
 	/*get index with key*/
 	index = key_index((const unsigned char *)key, table->size);
-
-	current_item = table->array[index];
 	table->array[index] = item;
-	if (current_item)
+	current_item = table->array[index];
+	if (current_item && current_item->key != (char*)key)
 	{
 		item->next = current_item;
 	}
