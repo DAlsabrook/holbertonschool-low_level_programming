dc650d1cf79c3d9489dce23adbe9af5bbc8617d5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 14:42:39 -0600

    =

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int row;
 	/*loop through table rows*/
-	for (row = 0; row < ht->size; row++)
+	for (row = 0; row <= ht->size; row++)
 	{
 		/*check if row is there*/
 		if (ht->array[row])
