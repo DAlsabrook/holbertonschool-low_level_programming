d649c6bf3cf4650b1b1a6358a4e7adbdd3cb34fe
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-11-16 12:15:31 -0600

    first attempt at task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,25 @@
 #include "hash_tables.h"
 /**
+ * hash_table_get - get a value from node with key
+ * @key: key to look for
+ * @ht: table to look through
  *
- *
- *
+ * Return: the value at that node
  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int row;
+	hash_node_t *current;
+
+	if (!ht || !key)
+		return (NULL);
+	for (row = 0; row < ht->size; row++)
+	{
+		current = ht->array[row];
+		if (current && strcmp((char *)key, current->key) == 0)
+		{
+			return (current->value);
+		}
+	}
+	return (NULL);
+}
